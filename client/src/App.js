df99723127b70241db40bd71c012442df4292bed
shadowmoonezios <agushifelde@hotmail.com>
2025-09-28 16:37:12 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Willkommen zur Reiseplaner-App</h1>
+      {/* Weitere Komponenten hier */}
+    </div>
+  );
+}
+
+export default App;
